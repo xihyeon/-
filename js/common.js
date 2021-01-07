685be5da4e75a206ddc0303a2126f07fd9706271
xihyeon <xihyeon.lee@gmail.com>
2021-01-07 18:18:07 +0900

    swiper fix

--- js/common.js
+++ js/common.js
@@ -4,4 +4,26 @@ $(function(){
     $(this).toggleClass("on");
     $("a").toggleClass(".active");
   });
+
+  // /*banner btn hover*/
+  // $(".banner-btn").click(function(){
+  //   $(".banner-btn").removeClass("on");
+  //   $(this).addClass("on");
+  // });
+
+  /*btn button*/
+  $("#slide_btn2").click(function(){
+    $("#slide_btn1").removeClass("on");
+    $("#slide1").removeClass("on");
+    $(this).addClass("on");
+    $("#slide2").addClass("on");
+  });
+  $("#slide_btn1").click(function(){
+    $("#slide_btn2").removeClass("on");
+    $("#slide2").removeClass("on");
+    $(this).addClass("on");
+    $("#slide1").addClass("on");
+  });
 });
+
+/*// document ready*/
